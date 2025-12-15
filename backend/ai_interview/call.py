a3b265f5a78e3706477627b645f1c1195ca61fc7
Daniel Samuel <danielsamuel@Daniels-MacBook-Pro.local>
2025-12-15 14:11:55 +0200

    work

--- backend/ai_interview/call.py
+++ backend/ai_interview/call.py
@@ -1,22 +1,24 @@
 import requests
 
 def get_ai_cv_data(jobid, appid):
-    url = "https://bdev.remoting.work/api/v1/jobdescription/ai-cv-data"
-    params = {
-        "jobid": jobid,
-        "appid": appid
-    }
+        url = "https://bdev.remoting.work/api/v1/jobdescription/ai-cv-data"
+        params = {
+            "jobid": jobid,
+            "appid": appid
+        }
 
-    try:
-        response = requests.get(url, params=params, timeout=10)
-        response.raise_for_status()   # Will raise HTTPError if status is 4xx or 5xx
+         # try:
+        # response = requests.get(url, params=params, timeout=10)
+        # response.raise_for_status()   # Will raise HTTPError if status is 4xx or 5xx
+        data = {'status': True, 'message': 'CV data retrieved successfully', 'data': {'job_description': {'title': '.Net Developer II', 'department': 96, 'location': '', 'employment_type': 'Full time', 'summary': '"<p style=\\"\\">We are seeking a highly skilled and experienced Full Stack Developer with a strong background in PHP (Laravel) and JavaScript (React.js) to join our development team. In this role, you will be responsible for designing, developing, and maintaining high-quality web applications that meet our clients\' needs and contribute to the success of our projects.</p><p style=\\"\\">Key Responsibilities:</p><p style=\\"\\">- Develop, test, and maintain robust, scalable, and high-performance web applications using PHP (Laravel) and JavaScript (React.js).</p><p style=\\"\\">- Collaborate with cross-functional teams, including designers, product managers, and other developers, to create seamless and engaging user experiences.</p><p style=\\"\\">-'
+                                                                                                                                                                                                      ' Write clean, maintainable, and efficient code following best practices and industry standards.</p><p style=\\"\\">- Troubleshoot, debug, and optimize existing code to ensure high availability and performance.</p><p style=\\"\\">- Participate in code reviews, providing constructive feedback to peers to maintain code quality.</p><p style=\\"\\">- Stay up-to-date with emerging technologies and industry trends to continuously improve skills and knowledge.</p><p style=\\"\\">- Assist in the development and implementation of APIs and third-party integrations.</p><p style=\\"\\">- Contribute to the architecture and design of new features and applications.</p><p style=\\"\\">Qualifications:</p><p style=\\"\\">- Bachelor\'s degree in Computer Science, Information Technology, or a related field (or equivalent work experience).</p><p style=\\"\\">- Proven experience as a Full Stack Developer, with a strong portfolio of web applications.</p><p style=\\"\\">- Proficiency in PHP and the Laravel framework.</p><p style=\\"\\">- Strong knowledge of JavaScript and experience with React.js.</p><p style=\\"\\">- Familiarity with front-end technologies such as HTML5, CSS3, and responsive design principles.</p><p style=\\"\\">- Experience with version control systems, preferably Git.</p><p style=\\"\\">- Understanding of RESTful APIs and asynchronous request handling.</p><p style=\\"\\">- Excellent problem-solving skills and attention to detail.</p><p style=\\"\\">- Strong communication and collaboration skills</p><p style=\\"\\">- Ability to work independently and as part of a team in a fast-paced environment.</p><p style=\\"\\">- Understanding of CI/CD processes is essential.</p><p style=\\"\\">Preferred Qualifications:</p><p style=\\"\\">- Experience with other JavaScript frameworks or libraries (e.g., Vue.js, Angular).</p><p style=\\"\\">- Knowledge of database management systems, particularly MySQL or PostgreSQL.</p><p style=\\"\\">- Experience with cloud platforms such as Azure is an added advantage.</p><p style=\\"\\">- Familiarity with containerization and orchestration tools (e.g., Docker, Kubernetes) is an added advantage.</p><p style=\\"\\">Benefits:</p><p style=\\"\\">- Competitive salary and performance-based bonuses.</p><p style=\\"\\">- Flexible working hours and remote work options.</p><p style=\\"\\">- Comprehensive health, dental, and vision insurance.</p><p style=\\"\\">- Professional development opportunities and continuous learning.</p><p style=\\"\\">- Friendly and collaborative work environment.</p><p style=\\"\\">- Opportunities to work on exciting and challenging projects.</p>"', 'responsibilities': '', 'requirements': [], 'skills': ['Software Development and Programming**r.w**Marketing/ Social Media Assistant']}, 'candidate_id': 201, 'candidate_email': 'testcan4@gmail.com', 'candidate_name': 'Max Well', 'job_id': 164, 'candidate_cv': {'education': [], 'experience': [], 'skills': ['Customer Service', 'Marketing/ Social Media Assistant', ' Administrative Assistant', ' Executive Assistant'], 'certifications': [], 'career_summary': 'candidate/546/resume/68def49f38383_1759442079.pdf'}}}
 
 
-        return response.json()
-        # return data
-    except requests.exceptions.RequestException as e:
-        print("Error making GET request:", e)
-        return None
+        # return response.json()
+        return data
+        # except requests.exceptions.RequestException as e:
+        #     print("Error making GET request:", e)
+        #     return None
 
 if __name__ == "__main__":
     data = get_ai_cv_data(jobid=164, appid=546)
