62e6b5f777492e4674a52b8434a2d29af937b611
Ubuntu <ubuntu@ip-172-31-34-5.eu-north-1.compute.internal>
2026-01-08 08:50:52 +0000

    working

--- agent-starter-react/next.config.ts
+++ agent-starter-react/next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: [
+    'heuristically-unpreventive-dimple.ngrok-free.dev',
+     'http://16.171.166.211',
+    'https://16.171.166.211',
+    // Add other ngrok domains here if needed
+    // Pattern: '*.ngrok-free.dev' or '*.ngrok.io' (if supported)
+  ],
 };
 
 export default nextConfig;
